93a6decfac33cc9e139cada029e414b218f58b5e
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-16 21:20:07 +0300

    Добавлены основные контракты данных

---
+++ MasterSystem/MasterSystem/DataContracts/Application.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using MasterSystem.Interfaces;
+using Newtonsoft.Json;
+
+namespace MasterSystem.DataContracts
+{
+    [DataContract]
+    public class Application : IDomainObject
+    {
+        [DataMember]
+        public string Number { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string Problem { get; set; }
+        [DataMember]
+        public string Contact { get; set; }
+        [DataMember]
+        public Service Service { get; set; }
+        
+        public string ConvertToJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+    }
+}
---
+++ MasterSystem/MasterSystem/DataContracts/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+using MasterSystem.Interfaces;
+using Newtonsoft.Json;
+
+namespace MasterSystem.DataContracts
+{
+    [DataContract]
+    public class Service : IDomainObject
+    {
+        [DataMember]
+        public string Number { get; set; }
+        
+        [DataMember]
+        public DateTime StartDate { get; set; }
+        
+        [DataMember]
+        public DateTime EndDate { get; set; }
+        
+        [DataMember]
+        public int ReactionTime { get; set; }
+        
+        [DataMember]
+        public int ResolvingTime { get; set; }
+        
+        public string ConvertToJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+    }
+}
