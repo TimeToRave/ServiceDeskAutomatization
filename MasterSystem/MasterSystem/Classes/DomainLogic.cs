760c2490c7d87e3c75ee3eee8def99f019e1f916
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-16 23:35:51 +0300

    Добавлен класс бизнес-логики

---
+++ MasterSystem/MasterSystem/Classes/DomainLogic.cs
@@ -0,0 +1,32 @@
+﻿using MasterSystem.DataContracts;
+using MasterSystem.Interfaces;
+
+namespace MasterSystem.Classes
+{
+    public class DomainLogic
+    {
+        private readonly ISynchronizationModule SyncModule;
+        private readonly IDataOperator DataOperator;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="dataOperator">Экземпляр класса, реализующий работу с данными</param>
+        /// <param name="syncModule">Экземпляр класса, реализующий механизмы интеграции со сторонней системой</param>
+        public DomainLogic(IDataOperator dataOperator, ISynchronizationModule syncModule)
+        {
+            DataOperator = dataOperator;
+            SyncModule = syncModule;
+        }
+
+        /// <summary>
+        /// Выполняет отправку обращения в стороннюю систему
+        /// </summary>
+        /// <param name="applicationNumber">Номер обращения</param>
+        public void SendApplication(string applicationNumber)
+        {
+            Application application = DataOperator.GetApplication(applicationNumber);
+            SyncModule.SendData(application);
+        }
+    }
+}
