343790990bbe4bb4ce4eec8e03e3188904800d36
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-16 21:19:25 +0300

    Добавлен класс, реализующий интерфейс IDataOperator, который заменяет работу с реальной базой данных

---
+++ MasterSystem/MasterSystem/Classes/FileDataOperator.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using MasterSystem.DataContracts;
+using MasterSystem.Interfaces;
+using Newtonsoft.Json;
+
+namespace MasterSystem.Classes
+{
+    public class FileDataOperator : IDataOperator
+    {
+        private string InputApplicationFile { get; }
+        private string InputServiceFile { get; }
+
+        public FileDataOperator(string inputApplicationFile, string inputServiceFile)
+        {
+            InputApplicationFile = inputApplicationFile;
+            InputServiceFile = inputServiceFile;
+        }
+        public Application GetApplication(string applicationNumber)
+        {
+            FileOperator fileOperator = new FileOperator();
+            string rawApplications = fileOperator.ReadTextFromFile(InputApplicationFile);
+            Application [] applicationCollection = JsonConvert.DeserializeObject<Application[]>(rawApplications);
+
+            Application result = applicationCollection.FirstOrDefault(app => app.Number == applicationNumber);
+            return result;
+        }
+    } 
+}
