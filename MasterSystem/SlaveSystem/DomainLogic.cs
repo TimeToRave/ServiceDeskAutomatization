b3f0e1c8a71a76c0edbab558bf9df1fcf09820b6
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-18 01:10:27 +0300

    Добавление проекта - вторая система, которая симулирует работу оператора

---
+++ MasterSystem/SlaveSystem/DomainLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using MasterSystem.DataContracts;
+using MasterSystem.Interfaces;
+
+namespace MasterSystem.Classes
+{
+    public class DomainLogic
+    {
+        private readonly ISynchronizationSender _syncSender;
+        private readonly IDataOperator DataOperator;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="dataOperator">Экземпляр класса, реализующий работу с данными</param>
+        /// <param name="syncSender">Экземпляр класса, реализующий механизмы интеграции со сторонней системой</param>
+        public DomainLogic(IDataOperator dataOperator, ISynchronizationSender syncSender)
+        {
+            DataOperator = dataOperator;
+            _syncSender = syncSender;
+        }
+
+        static Semaphore sem = new Semaphore(3, 3);
+
+        public void SaveApplication(Application application)
+        {
+            Thread.Sleep(1000 * application.Service.ReactionTime);
+            
+            Thread th = new Thread(() =>
+            {
+
+            sem.WaitOne();
+
+            Console.WriteLine($"Принято {application.Title}");
+
+            
+            application.Status = "InProgress";
+            _syncSender.SendData(application);
+
+            Thread.Sleep(1000 * application.Service.ResolvingTime);
+            application.Status = "Completed";
+            _syncSender.SendData(application);
+
+            sem.Release();
+        }
+        );
+        th.Name = $"Thread ID: {Guid.NewGuid()}";
+        th.Start();
+    }
+}
+
+}
---
+++ MasterSystem/SlaveSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using MasterSystem.Classes;
+using MasterSystem.Interfaces;
+
+namespace SlaveSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            IDataOperator fileDataOperator = new FileDataOperator("ApplicationsInput.json", "ServicesInput.json");
+            
+            ISynchronizationSender synchronyzationSender =
+                new RabbitMqSynchronizationSender("ServiceDeskSlave", "ServiceDeskSlaveToMaster", "");
+            ISynchronizationSubscriber synchronizationSubscriber =
+                new RabbitMqSynchronizationSubscriber("ServiceDeskMasterToSlave");
+
+            var domainLogic = new DomainLogic(fileDataOperator, synchronyzationSender);
+
+            synchronizationSubscriber.SubscribeOn(domainLogic);
+
+            Console.ReadKey();
+        }
+    }
+}
